99159939d6af1ce5095f1562693d536fb3754e1e
Sergio C Castelani Jr <sergio.castelani@gmail.com>
2023-10-28 10:01:35 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Magic Library
+React project for the course Starter FullStack from Growdev (https://growdev.com.br)
---
+++ src/Components/App/index.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+export function App() {
+  const [bookList, setBookList] = useState([])
+
+  return (
+    <>
+    </>
+  )
+}
